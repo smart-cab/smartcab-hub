c70df5898517e474074f6c689afbb55956a6d59f
Alchemmist <anton.ingrish@gmail.com>
2023-09-10 11:21:58 +0300

    fix bug in header

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,9 @@
+const _DateAndTime = () => {
+  document.querySelector("#currentTime").innerHTML = new Date().toLocaleTimeString('en-gb', {hour: 'numeric', minute: 'numeric', hour12: false});
+}
+setInterval(_DateAndTime, 60000);
+_DateAndTime();
+/*
 var today = new Date();
 var time = ""
 if (today.GetHours < 10) {
@@ -10,3 +16,4 @@ if (today.getMinutes() < 10) {
 time += today.getMinutes()
 
 document.getElementById('date-time').innerHTML=time;
+*/
