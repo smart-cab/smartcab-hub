34d5701333d2a32421d826d0491271674ebcb9ec
Alchemmist <anton.ingrish@gmail.com>
2023-11-05 23:20:26 +0300

    done frontend for tab page element

--- frontend/src/components/ControlPage.js
+++ frontend/src/components/ControlPage.js
@@ -1,20 +1,64 @@
 import React from "react";
 import Header from "./Header.js";
+import { Grid, Paper, Typography } from '@mui/material';
 import MySwitch from "./Switch.js";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import "./ControlPage.scss";
+import { makeStyles } from "@material-ui/core";
 
 
 function ControlPage() {
+    const paperStyle = {
+        display: "flex",
+        backgroundColor: 'rgba(202, 200, 200, 0.4)', // Измените на ваш желаемый цвет фона
+        padding: "16",
+        textAlign: 'center',
+        padding: "16px",
+        textAlign: "center",
+        marginTop: "10%",
+        height: "3cm",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        borderRadius: "15px",
+        boxShadow: "0px 0px 0px 0px",
+        fontSize: "20px",
+        gap: "10px",
+    };
 
     return (
         <div>
             <Header />
-            <div style={ {marginLeft: "70px"} }>
-                <Container fluid="md">
-                    <MySwitch />
-                </Container>
+            <div className="TabPage">
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Paper style={paperStyle}>
+                            <p>Лампа LW204</p>
+                            <MySwitch />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper style={paperStyle}>
+                            <p>Свет 2</p>
+                            <MySwitch />
+                        </Paper>
+                    </Grid>
+                </Grid>
+
+
+
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Paper style={paperStyle}>
+                            <p>Свет 1</p>
+                            <MySwitch />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper style={paperStyle}>
+                            <MySwitch />
+                        </Paper>
+                    </Grid>
+                </Grid>
             </div>
         </div>
 
