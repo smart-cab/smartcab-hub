04b9327bc39947f5e8bc6d0ad5b2e688875765ea
Alchemmist <anton.ingrish@gmail.com>
2023-11-06 12:28:14 +0300

    upadte interface: slider and sidebar

--- frontend/src/components/controls/Slider.jsx
+++ frontend/src/components/controls/Slider.jsx
@@ -5,11 +5,15 @@ import "./Slider.scss";
 
 function MySlider({min, max, current_value=(max - min)/2, step=1}) {
     return (
-        <div>
+        <div >
             <tc-range-slider 
-                slider-width="11em"
-                slider-bg="lightgrey"
-                slider-bg-fill="#81D7DD"
+                slider-width = "11em"
+                slider-bg = "lightgrey"
+                slider-bg-fill = "#81D7DD"
+                slider-height="0.6rem"
+                pointer-radius="1rem"
+                pointer-width="0.7rem"
+                pointer-height="1.8rem"
                 
                 min = {`${min}`}
                 max = {`${max}`}
--- frontend/src/components/layout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../Sidebar.jsx";
 
 const AppLayout = () => {
     return <div style={{
-        padding: '0px 0px 0px 250px'
+        padding: '0px 0px 0px 250px',
     }}>
         <Sidebar />
         <Outlet />
