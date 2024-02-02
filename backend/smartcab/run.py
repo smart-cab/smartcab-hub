966ec5743c36be7852e600ec1a3a5a8cc6a554ab
Mark Meliksetyan <markmelix@gmail.com>
2024-02-02 13:18:57 +0300

    backend: throw 404 if device or interface not found

--- backend/smartcab/run.py
+++ backend/smartcab/run.py
@@ -1,7 +1,7 @@
 from threading import Thread
 
 from dotenv import find_dotenv, load_dotenv
-from flask import request
+from flask import abort, request
 
 import smartcab
 from smartcab import interface
@@ -18,26 +18,43 @@ load_dotenv(find_dotenv())
 app = smartcab.make_app()
 
 
-@app.route("/new_vote", methods=["GET",  "POST"])
+@app.route("/new_vote", methods=["GET", "POST"])
 def new_voice():
     vote = request.args.get("vote")
     with db.session() as db_sess:
         type_id = db_sess.query(EvalType).filter(EvalType.eval_type == vote).first().id
         db_sess.add(Lesson(eval_id=type_id))
         db_sess.commit()
-    return {'status': 'ok'}
+    return {"status": "ok"}
 
 
 @app.route("/device/<device_id>", methods=["GET"])
 def mqtt_get(device_id):
-    return {"status": "ok"} | DEVICES[device_id].get_interface(interface.MQTT).get_data()
+    device = DEVICES.get(device_id, None)
+    if device is None:
+        abort(404)
+
+    mqtti = device.get_interface(interface.MQTT)
+    if mqtti is None:
+        abort(404)
+
+    return {"status": "ok"} | mqtti.get_data()
 
 
 @app.route("/device/<device_id>", methods=["POST"])
 def mqtt_publish(device_id):
+    device = DEVICES.get(device_id, None)
+    if device is None:
+        abort(404)
+
+    mqtti = device.get_interface(interface.MQTT)
+    if mqtti is None:
+        abort(404)
+
     field = request.args.get("field", None)
     value = request.args.get("value")
-    DEVICES[device_id].get_interface(interface.MQTT).set(value, field)
+
+    mqtti.set(value, field)
     return {"status": "ok"}
 
 
