e345576a468f9d051a6e942022a22ce5024b56d7
Mark Meliksetyan <markmelix@gmail.com>
2024-02-03 14:38:33 +0300

    try fixing babel related issues again

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -21,3 +21,17 @@ module.exports = function override(config) {
     ])
     return config;
 }
+
+module.rules = [
+    {
+        test: path.join(__dirname, '.'),
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env',
+                '@babel/react', {
+                    'plugins': ['@babel/plugin-proposal-class-properties']
+                }]
+        }
+    }
+];
