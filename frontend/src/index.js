0e1ee30d4de7c4c3c70def9996c7d09dc8f9732b
Mark Meliksetyan <markmelix@gmail.com>
2024-02-03 13:26:23 +0300

    frontend: use react 18 renderer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './pages/App';
 import "./index.scss";
+import { createRoot } from 'react-dom/client';
 
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+      <App />
+    </React.StrictMode>
 );
-
