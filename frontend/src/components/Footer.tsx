22d23156776a8148ad8e478015cf2446cd58d09d
Mark Meliksetyan <markmelix@gmail.com>
2024-03-16 20:07:00 +0300

    create call security function stub

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,15 @@
 import MyButton from "./controls/Button";
+import callSecurity from "./VoIP";
 import "./Footer.scss";
 
 export default function Footer() {
     return (
         <header className="fixed-bottom Footer">
-            <MyButton text={"SOS"} button_type={"ButtonRed"} />
+            <MyButton
+                text={"SOS"}
+                button_type={"ButtonRed"}
+                hook={callSecurity}
+            />
         </header>
     );
 }
