6a3ec83176fee059bfa3062089cea39c0120fc29
Mark Meliksetyan <markmelix@gmail.com>
2024-03-19 12:19:02 +0300

    react-sip sucks

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,10 @@
-import MyButton from "./controls/Button";
-import callSecurity from "./VoIP";
+import SOSButton from "./VoIP";
 import "./Footer.scss";
 
 export default function Footer() {
     return (
         <header className="fixed-bottom Footer">
-            <MyButton
-                text={"SOS"}
-                button_type={"ButtonRed"}
-                hook={callSecurity}
-            />
+            <SOSButton />
         </header>
     );
 }
--- frontend/src/components/VoIP.tsx
+++ frontend/src/components/VoIP.tsx
@@ -1,3 +1,43 @@
-export default function callSecurity() {
-    return;
+import MyButton from "./controls/Button";
+import { SipProvider } from "react-sip";
+
+export default function SOSButton() {
+    return (
+        <SipProvider
+            host="192.168.0.51"
+            port={8089}
+            pathname="/asterisk/ws" // Path in socket URI (e.g. wss://sip.example.com:7443/ws); "" by default
+            user="103"
+            password="NnBJeGFVUU5KSU09" // usually required (e.g. from ENV or props)
+            autoRegister={true} // true by default, see jssip.UA option register
+            autoAnswer={false} // automatically answer incoming calls; false by default
+            iceRestart={false} // force ICE session to restart on every WebRTC call; false by default
+            sessionTimersExpires={120} // value for Session-Expires header; 120 by default
+            /* extraHeaders={
+             *     {
+             *         // optional sip headers to send
+             *         register: ["X-Foo: foo", "X-Bar: bar"],
+             *         invite: ["X-Foo: foo2", "X-Bar: bar2"],
+             *     }
+             * } */
+            /* iceServers={[
+             *     // optional
+             *     { urls: ["stun:a.example.com", "stun:b.example.com"] },
+             *     {
+             *         urls: "turn:example.com",
+             *         username: "foo",
+             *         credential: "1234",
+             *     },
+             * ]} */
+            debug={false} // whether to output events to console; false by default
+        >
+            <MyButton
+                text={"SOS"}
+                button_type={"ButtonRed"}
+                hook={() => {
+                    SipProvider.prototype.getChildContext().startCall("103");
+                }}
+            />
+        </SipProvider>
+    );
 }
