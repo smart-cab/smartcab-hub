e27aa77c9cbccae3f43ccd7331ff1f1693098687
Mark Meliksetyan <markmelix@gmail.com>
2024-04-15 07:28:44 +0300

    predend devmap redis keys with 'device' prefix
    Needed to make Redis used not only for devmap but for settings and other
    stuff too.

--- backend/smartcab/dev.py
+++ backend/smartcab/dev.py
@@ -17,26 +17,27 @@ class DeviceMap:
         with open(path) as file:
             object = yaml.load(file, Loader=yaml.Loader)
             for key, value in object.items():
+                key = f"device:{key}"
                 self._db.hset(key, mapping=value)
                 self._db.hset(key, "data", "{}")
 
     def get_data(self, device_id, interface_id):
-        name = f"{device_id}/{interface_id}"
+        name = f"device:{device_id}/{interface_id}"
         return json.loads(self._db.hget(name, "data"))
 
     def update_data(self, device_id, interface_name, data):
-        name = f"{device_id}/{interface_name}"
+        name = f"device:{device_id}/{interface_name}"
         self._db.hset(name, "data", json.dumps(data, indent=None))
 
     def _get_interface(self, name):
         return _INTERFACE_MAPPINGS[name.split("/")[1]](**self._db.hgetall(name))
 
     def get_interface(self, device_id: str, interface_name: str):
-        name = f"{device_id}/{interface_name}"
+        name = f"device:{device_id}/{interface_name}"
         return self._get_interface(name)
 
     def interfaces(self, interface_name: str):
-        for name in self._db.keys(f"*/{interface_name}"):
+        for name in self._db.keys(f"device:*/{interface_name}"):
             yield self._get_interface(name)
 
 
